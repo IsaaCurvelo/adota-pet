b1b8e8c1dca1103365b72e3f1c5cca174646e082
Isaac Francis <isaacfrancis-cm@hotmail.com>
2019-11-24 23:43:25 -0300

    implementação de métodos para busca do animal por dono

--- src/main/java/br/uema/pecs/adotapet/repository/Animals.java
+++ src/main/java/br/uema/pecs/adotapet/repository/Animals.java
@@ -1,9 +1,12 @@
 package br.uema.pecs.adotapet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.uema.pecs.adotapet.model.Animal;
+import br.uema.pecs.adotapet.model.Usuario;
 
 public interface Animals extends JpaRepository<Animal, Integer> {
-
+	public List<Animal> findByDoador(Usuario usuario);
 }
--- src/main/java/br/uema/pecs/adotapet/resource/AnimalResource.java
+++ src/main/java/br/uema/pecs/adotapet/resource/AnimalResource.java
@@ -1,12 +1,14 @@
 package br.uema.pecs.adotapet.resource;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +18,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.uema.pecs.adotapet.model.Animal;
+import br.uema.pecs.adotapet.model.Usuario;
 import br.uema.pecs.adotapet.repository.Animals;
+import br.uema.pecs.adotapet.repository.Usuarios;
 import br.uema.pecs.adotapet.storage.Storage;
 
 @RestController
@@ -25,6 +29,9 @@ public class AnimalResource {
 
 	@Autowired
 	private Animals animals;
+
+	@Autowired
+	private Usuarios usuarios;
 
 	@Autowired
 	private Storage storage;
@@ -55,6 +62,12 @@ public class AnimalResource {
 	@GetMapping
 	public Page<Animal> listar(Pageable pageable) {
 		return this.animals.findAll(pageable);
+	}
+
+	@GetMapping("/usuario/{usuarioId}")
+	public List<Animal> listarPorUsuario(@PathVariable("usuarioId") Integer usuarioId) {
+		Usuario usuario = usuarios.findById(usuarioId).get();
+		return this.animals.findByDoador(usuario);
 	}
 
 }
