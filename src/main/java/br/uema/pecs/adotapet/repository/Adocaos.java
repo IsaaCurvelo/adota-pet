b1942bf8f30c8e8b19decf2baf066015da6b3bb6
Isaac Francis <isaacfrancis-cm@hotmail.com>
2019-12-04 23:43:05 -0300

    implementacao de todas as funcionalidades basicas do sistema

--- src/main/java/br/uema/pecs/adotapet/repository/Adocaos.java
+++ src/main/java/br/uema/pecs/adotapet/repository/Adocaos.java
@@ -1,9 +1,22 @@
 package br.uema.pecs.adotapet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.uema.pecs.adotapet.model.Adocao;
+import br.uema.pecs.adotapet.model.Animal;
+import br.uema.pecs.adotapet.model.Usuario;
 
 public interface Adocaos extends JpaRepository<Adocao, Integer> {
-
+	@Query(value = "select a from Adocao a where a.doador = :usuario and a.aConfirmar = true")
+	public List<Adocao> findAdocaoPendentePorUsuario(@Param("usuario") Usuario usuario);
+	
+	@Modifying
+	
+	@Query(value = "delete from Adocao a where a.animal = :animal and a.aConfirmar = true")
+	public void deletaAdocaoPendenteAnimal(@Param("animal") Animal animal);
 }
--- src/main/java/br/uema/pecs/adotapet/resource/AdocaoResource.java
+++ src/main/java/br/uema/pecs/adotapet/resource/AdocaoResource.java
@@ -1,9 +1,14 @@
 package br.uema.pecs.adotapet.resource;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,8 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.uema.pecs.adotapet.model.Adocao;
 import br.uema.pecs.adotapet.model.Animal;
+import br.uema.pecs.adotapet.model.Usuario;
 import br.uema.pecs.adotapet.repository.Adocaos;
 import br.uema.pecs.adotapet.repository.Animals;
+import br.uema.pecs.adotapet.repository.Usuarios;
 
 @RestController
 @RequestMapping("adocaos")
@@ -26,6 +33,15 @@ public class AdocaoResource {
 	@Autowired
 	private Animals animals;
 
+	@Autowired
+	private Usuarios usuarios;
+
+	@GetMapping("/usuario/{usuarioId}")
+	public List<Adocao> adocoesaConfirmar(@PathVariable Integer usuarioId) {
+		Usuario usuario = this.usuarios.findById(usuarioId).get();
+		return this.adocaos.findAdocaoPendentePorUsuario(usuario);
+	}
+
 	@PostMapping
 	public Adocao salvar(@RequestBody Adocao adocao) {
 		adocao.setDataAdocao(new Date());
@@ -33,8 +49,22 @@ public class AdocaoResource {
 		return this.adocaos.save(adocao);
 	}
 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Integer id) {
+		Optional<Adocao> optionalAdocao = adocaos.findById(id);
+
+		if (!optionalAdocao.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		adocaos.delete(optionalAdocao.get());
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@Transactional
 	@PutMapping("/confirmar/{adocaoId}")
-	public ResponseEntity<Void> confirmarAdocao(@PathVariable Integer adocaoId) {
+	public List<Adocao> confirmarAdocao(@PathVariable Integer adocaoId) {
 		Adocao adocao = adocaos.findById(adocaoId).get();
 		Animal animalExistente = this.animals.findById(adocao.getAnimal().getId()).get();
 
@@ -48,9 +78,11 @@ public class AdocaoResource {
 
 		adocao.setaConfirmar(false);
 		adocao.setDataAdocao(new Date());
-		this.adocaos.save(adocao);
 
-		return ResponseEntity.noContent().build();
+		this.adocaos.save(adocao);
+		this.adocaos.deletaAdocaoPendenteAnimal(adocao.getAnimal());
+
+		return this.adocaos.findAdocaoPendentePorUsuario(adocao.getDoador());
 	}
 
 }
